bc6306d285218b6f85e5b73d2a17f8414926543a
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-03-20 10:53:07 -0400

    Add a type column to a User model; implement method for customer/business

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def customer?
+    type == 'Customer'
+  end
+
+  def seller?
+    type == 'Business'
+  end
 end
