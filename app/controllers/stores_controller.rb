e78e4236dee2709918189e14ccf66a7277260fe3
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-03-23 19:04:53 -0400

    Comment Nazim's code

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,31 +1,22 @@
 class StoresController < ApplicationController
   require 'rqrcode'
- #This is still in progress
-  def storeqrcode
-    @store = Store.last
-    @code = @store.code
+  # This is still in progress
+  # def storeqrcode
+  #   @store = Store.last
+  #   @code = @store.code
 
-    @storeqr = RQRCode::QRCode.new(@store.code)
-  end
-
-  
+  #   @storeqr = RQRCode::QRCode.new(@store.code)
+  # end
 
   def update
-
   end
 
   def edit
-
   end
 
   def show
-
   end
 
-  private
-
-  def qr_params
-    
-  end
-
+  # def qr_params
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  # def customer?
-  #   type == 'Customer'
-  # end
-
-  # def business?
-  #   type == 'Business'
-  # end
 end
