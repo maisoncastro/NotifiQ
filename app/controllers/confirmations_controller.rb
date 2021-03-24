63b6c8b950342381bc28bc51f3cbab2fca4b618f
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-03-23 20:09:11 -0400

    Add confirmations actions

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,10 +1,31 @@
 class ConfirmationsController < ApplicationController
   def new
+    @conifrmation = Confirmation.new
+  end
+
+  def create
+    @conifrmation = Confirmation.new(confirmation_params)
+    @conifrmation.customer = current_customer
+    @store = Store.find(params[:store_id])
+    @conifrmation.store = @store
+    if @conifrmation.save
+      redirect_to confirmations_index_path
+    else
+      render :new
+    end
   end
 
   def show
+    @confirmation = Confirmation.new
   end
 
   def index
+    @confirmations = Confirmation.all
+  end
+
+  private
+
+  def confirmation_params
+    params.require(:confirmation).permit(:start_time, :end_time, :position, :qrcode)
   end
 end
