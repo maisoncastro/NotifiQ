dfec9286770b257d9df54efb0393ed3e67d17ddb
tep yith <tep.yith@gmail.com>
2021-03-25 20:14:51 -0400

    Seeding, getting new pull request with Confirmation views.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
+
+puts "Deleting database.."
 User.destroy_all
 Store.destroy_all
 
+puts "Creating Users.."
+
+one = User.create!(fname: "one", lname: "last", email: "one@email.com", password: "123456", type: "Customer")
+
+puts "Creating Business.."
 u = BusinessUser.create!(email: 'a@a.a', password: '123456')
 
+puts "Creating Store.."
 s = Store.new(name: 'test')
 s.user = u
 s.save!
 
+puts "Done!"
