e2f0831da0f4226596b030fe54e652736083021a
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-03-30 20:38:54 -0400

    Fix typo in class name

--- test/mailers/previews/confirmation_mailer_preview.rb
+++ test/mailers/previews/confirmation_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/confirmation_mailer
 class ConfirmationMailerPreview < ActionMailer::Preview
   def confirmation_mail_preview
-    ExampleMailer.sample_email(User.first)
+    ConfirmationMailer.sample_email(User.first)
   end
 end
