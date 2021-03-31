c5d0ea83bf1784c4bc7a145cf76e9644a82bf923
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-03-30 20:38:24 -0400

    Add test file to test mailers

--- test/mailers/previews/confirmation_mailer_preview.rb
+++ test/mailers/previews/confirmation_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/confirmation_mailer
 class ConfirmationMailerPreview < ActionMailer::Preview
-
+  def confirmation_mail_preview
+    ExampleMailer.sample_email(User.first)
+  end
 end
