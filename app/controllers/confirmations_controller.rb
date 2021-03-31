c1bc88c4f619eaaec1eb9cabe0282f219814b5d9
tep yith <tep.yith@gmail.com>
2021-03-31 17:15:42 -0400

    Added nested routes to confirmations and stores.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,24 +1,4 @@
 class ConfirmationsController < ApplicationController
-  def new
-    @confirmation = Confirmation.new
-  end
-
-  def create
-    @confirmation = Confirmation.new(confirmation_params)
-    @confirmation.customer = current_customer_user
-    @store = Store.find(params[:store_id])
-    @confirmation.store = @store
-    if @confirmation.save
-      redirect_to confirmations_index_path
-    else
-      render :new
-    end
-  end
-
-  def show
-    @confirmation = Confirmation.find(params[:id])
-  end
-
   def index
     @confirmations = Confirmation.all
 
@@ -30,6 +10,28 @@ class ConfirmationsController < ApplicationController
       shape_rendering: 'crispEdges',
       module_size: 4
     )
+  end
+
+  def show
+    @confirmation = Confirmation.find(params[:id])
+    @store = Store.find(params[:store_id])
+  end
+
+  def new
+    @confirmation = Confirmation.new
+  end
+
+  def create
+    @confirmation = Confirmation.new(confirmation_params)
+    @confirmation.customer = current_customer_user
+    @store = Store.find(params[:store_id])
+    @confirmation.store = @store
+    @confirmation.user = current_user
+    if @confirmation.save
+      redirect_to confirmations_index_path
+    else
+      render :new
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,14 @@
 Rails.application.routes.draw do
-  
-  # get 'stores/qrcode'
-  # get 'stores/update'
-  # get 'stores/:id/edit', to: 'stores#edit'
-  # get 'stores/:id/show', to: 'stores#show'
-  # get 'stores/new'
-  # post 'stores/create'
-  resources :stores, only: [:new, :create, :update, :edit, :show]
-  resources :confirmations, only: [:new, :create, :show, :index]
   root to: 'pages#home'
   get 'dashboard/index'
-  # get 'confirmations/new'
-  # post 'confirmations/create'
-  # get 'confirmations/show'
-  # get 'confirmations/index'
+
+  resources :stores, only: [:new, :create, :update, :edit, :show] do
+    resources :confirmations, only: [:new, :create, :show, :index]
+  end
+
+
   devise_for :customers, path: 'customers'
   devise_for :business_users, path: 'business_users'
-   
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
