3275c3038d523b3b63d79257794b39b2b9959051
Mary Grace Nazareno <mgracenazareno@gmail.com>
2021-04-06 19:51:58 -0400

    Display map with marker

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,11 +3,13 @@ class Store < ApplicationRecord
   has_many :confirmations
   validates :name, uniqueness: true
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   after_create :store_qrcode1
 
   def store_qrcode1
     self.store_qrcode = SecureRandom.hex
     save
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  
+
   # get 'stores/qrcode'
   # get 'stores/update'
   # get 'stores/:id/edit', to: 'stores#edit'
   # get 'stores/:id/show', to: 'stores#show'
   # get 'stores/new'
   # post 'stores/create'
-  resources :stores, only: [:new, :create, :update, :edit, :show]
+  resources :stores, only: [:index, :new, :create, :update, :edit, :show]
   resources :confirmations, only: [:new, :create, :show, :index]
   root to: 'pages#home'
   get 'dashboard/index'
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   # get 'confirmations/index'
   devise_for :customers, path: 'customers'
   devise_for :business_users, path: 'business_users'
-   
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
