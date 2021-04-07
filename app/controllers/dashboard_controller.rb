a0d25226604c765c7be852ea8141d9faddff163d
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2021-04-06 20:51:57 -0400

    Created QR Code + Card

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,13 @@ class DashboardController < ApplicationController
   end
 
   def settings
+    @store_qrcode = RQRCode::QRCode.new("notifiq.herokuapp.com/stores/1/confirmations/new")
+
+    @svg = @store_qrcode.as_svg(
+      offset: 0,
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 4
+    )
   end
 end
