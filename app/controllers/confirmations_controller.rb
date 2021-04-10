3a6e026cd114fa035cdb6b4d2d5f8c5845ccff8e
Michael Batrakov <mykhailobatrakov@gmail.com>
2021-04-10 13:53:37 -0400

    Add counter to tack people in a queue as well as approx time

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,22 +1,32 @@
 class ConfirmationsController < ApplicationController
   def index
-    @confirmations = Confirmation.all
+    confirmations = Confirmation.all
+    @user_confirmations = confirmations.select { |confirmation| confirmation.user_id == current_user.id }
+  end
 
-    @store_qrcode = RQRCode::QRCode.new("http://localhost:3030/confirmations/")
-
-    @svg = @store_qrcode.as_svg(
-      offset: 0,
-      color: '000',
-      shape_rendering: 'crispEdges',
-      module_size: 4
-    )
+  def edit
+    @confirmation = Confirmation.find(params[:id])
+    @confirmation.completed = true
+    @confirmation.save!
   end
 
   def show
     @confirmation = Confirmation.find(params[:id])
     @store = Store.find(params[:store_id])
+    completed_confirmations = @store.confirmations.select { |confirmation| confirmation.completed }
+    @current_position = @confirmation.position - completed_confirmations.count
+    @time = Time.new + (10 * @current_position)
 
-    @store_qrcode = RQRCode::QRCode.new(destroy_customer_session_path)
+    # @store_qrcode = RQRCode::QRCode.new(destroy_customer_session_path)
+    # @svg = @store_qrcode.as_svg(
+    #   offset: 0,
+    #   color: '000',
+    #   shape_rendering: 'crispEdges',
+    #   module_size: 4
+    # )
+
+    @url = "stores/#{@store.id}/confirmations/#{@confirmation.id}/edit"
+    @store_qrcode = RQRCode::QRCode.new("http://localhost:3030/#{@url}")
 
     @svg = @store_qrcode.as_svg(
       offset: 0,
@@ -29,6 +39,11 @@ class ConfirmationsController < ApplicationController
   def new
     @confirmation = Confirmation.new
     @store = Store.find(params[:store_id])
+    pending_confirmations = @store.confirmations.reject { |confirmation| confirmation.completed }
+    @current_position = pending_confirmations.count
+    @average_wait_time = 10 * @current_position
+
+    # raise
   end
 
   def create
@@ -36,12 +51,12 @@ class ConfirmationsController < ApplicationController
     @store = Store.find(params[:store_id])
     @confirmation.store = @store
     @confirmation.user = current_user
-    if @confirmation.save
+    # @store.increment(:customer_count)
+    @confirmation.position = @store.confirmations.count + 1
+    if @confirmation.save!
       ConfirmationMailer.confirmation_email(current_user).deliver_now
-
       redirect_to confirmations_path
     else
-
       render :new
     end
   end
