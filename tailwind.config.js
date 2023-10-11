a78b3c7b87758c30a531eb27c25d0e7b12486c2d
Nazim-Yoldas Sesen <nazimsesen@gmail.com>
2023-10-10 21:48:28 -0400

    Fixed Gemfiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit", // Enable JIT mode
   content: ["./app/components/**/*.{erb,html}", "./app/views/**/*.{erb,html}"],
   theme: {
     extend: {},
